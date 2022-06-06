d99fe2f00dc00bd77fe92357772d052af02472cb
rickyt49 <raiztran49@gmail.com>
2022-06-06 21:18:51 +0700

    first push no content

---
+++ src/test/java/com/axonactive/phonestore/PhonestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axonactive.phonestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
